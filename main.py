e2c848962d4dd9977eb33ec48d6de2b9a976b46f
Deep Suchak <you@example.com>
2023-12-19 00:12:50 -0500

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import *
+from cnnClassifier.pipeline.stage_02_prepare_base_model import *
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -15,3 +18,17 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare base model stage"
+try:
+    logger.info(f"*********************")
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
